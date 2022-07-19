364b73608232a86223aa7f51160f97318896a84b
Ahmad Khoirunnufus <a.khoirunnufus@gmail.com>
2022-07-19 20:18:35 +0700

    download google drive file

--- web_service/controllers/FileController.php
+++ web_service/controllers/FileController.php
@@ -5,6 +5,9 @@ namespace quicknav\controllers;
 use Yii;
 use yii\web\Controller;
 use yii\filters\auth\HttpBearerAuth;
+use quicknav\models\User;
+use Google\Client;
+use Google\Service\Drive;
 use quicknav\components\DriveFile;
 
 class FileController extends Controller
@@ -40,6 +43,44 @@ class FileController extends Controller
     $data = array_merge($folders, $files);    
     
     return $this->asJson($data);
+  }
+
+  public function actionDownload()
+  {
+    $request = Yii::$app->request;
+    $paramFileId = $request->get('file_id');
+    $paramFileName = $request->get('file_name');
+    $paramFileMimeType = $request->get('file_mime_type');
+
+    // $identity = Yii::$app->user->identity;
+    $identity = User::findOne(['email' => 'omanaristarihoran33@gmail.com']);
+
+    $access_token = $identity->g_access_token;
+    $access_token = json_decode($access_token, true);
+    $client_secret = Yii::getAlias('@app/client_secret.json');    
+    
+    try {
+      $client = new Client();
+      $client->setAuthConfig($client_secret);
+      $client->setAccessToken($access_token);
+      $drive = new Drive($client);
+      
+      $response = $drive->files->get($paramFileId, [
+        'alt' => 'media'
+      ]);
+      $content = $response->getBody()->getContents();
+
+      return Yii::$app->response->sendContentAsFile(
+        $content, 
+        $paramFileName,
+        [
+          'mimeType' => $paramFileMimeType,
+          'inline' => false,
+        ],
+      );
+    } catch(Exception $e) {
+      echo "Terjadi kesalahan :(";
+    }
   }
 
   public function actionTest()
